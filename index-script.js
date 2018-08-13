0fc97fde4b72356c070fcc9848cacfaa8405deb7
nick redmond <nickredmond@tradewarriorsoftware.com>
2018-08-12 22:12:41 -0600

    added selling point at bottom w/ mobile graphic, and added php file for deploying to Heroku

--- index-script.js
+++ index-script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
   $('.parallax').parallax();
 });
+
+var navigate = function(fromViewName, toViewName) {
+	document.getElementById(fromViewName + "-view").display = "none";
+	document.getElementById(toViewName + "-view").display = "block";
+};
